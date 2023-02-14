60ac50d3d1129bed44a64dd092c50836ea5544d0
dongkya <kdh2395478@gmail.com>
2023-02-14 13:31:08 -0700

    z

--- 7 DS Workflows Hands On/exploration hands on.R
+++ 7 DS Workflows Hands On/exploration hands on.R
@@ -5,3 +5,27 @@ df2 <- read_csv('https://www.dropbox.com/s/xfp1qzxvo19ym0x/df_2.csv?dl=1')
 df3 <- read_csv('https://www.dropbox.com/s/uzusr9723ffn546/df_3.csv?dl=1')
 df4 <- read_csv('https://www.dropbox.com/s/js8tehtsk7btpeq/df_4.csv?dl=1')
 
+df1 %>% summarize_all(list(mean=mean, sd=sd))
+df2 %>% summarize_all(list(mean=mean, sd=sd))
+df3 %>% summarize_all(list(mean=mean, sd=sd))
+df4 %>% summarize_all(list(mean=mean, sd=sd))
+
+
+
+df1 %>% summarize_all(list(mean=mean, sd=sd))
+df1 %>% cor()
+lm(y ~ x, data = df1) %>% coefficients()
+
+df1 %>% 
+  ggplot(aes(x=x, y=y)) +
+  geom_point()
+
+bind_rows(df1, df2, df3, df4, .id = 'df') %>% 
+  ggplot (aes (x = x, fill = df)) +
+  geom_density()
+
+bind_rows(df1, df2, df3, df4, .id = 'df') %>% 
+  ggplot (aes (x = x, y = y, color = df)) +
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  facet_wrap(~df)
